5a32d2f7bd19194ebdfd995691ee792445bc828a
Peter Maatman <blackwolf12333@gmail.com>
2015-11-28 00:53:08 +0100

    update menu

--- app/src/main/java/me/blackwolf12333/appcki/MainActivity.java
+++ app/src/main/java/me/blackwolf12333/appcki/MainActivity.java
@@ -99,17 +99,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_news) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_agenda) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_poll) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_roephoek) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_gol) {
 
         } else if (id == R.id.nav_login) {
             Intent loginIntent = new Intent(this, LoginActivity.class);
