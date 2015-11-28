4c023ebdd1e2b94cc4811dd771f4e0b6675cccf7
Peter Maatman <blackwolf12333@gmail.com>
2015-11-28 02:07:58 +0100

    misc changes

--- app/src/main/java/me/blackwolf12333/appcki/MainActivity.java
+++ app/src/main/java/me/blackwolf12333/appcki/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Roephoek", null).show(); //TODO change to roephoek activity
             }
         });
 
