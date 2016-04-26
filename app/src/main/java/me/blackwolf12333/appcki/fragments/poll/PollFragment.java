4f0949b87dee0799b6d921656d0c6f04e5396ab8
Peter Maatman <blackwolf12333@gmail.com>
2016-04-26 20:24:39 +0200

    cleanup

--- app/src/main/java/me/blackwolf12333/appcki/fragments/poll/PollFragment.java
+++ app/src/main/java/me/blackwolf12333/appcki/fragments/poll/PollFragment.java
@@ -1,12 +1,17 @@
 package me.blackwolf12333.appcki.fragments.poll;
 
+
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.greenrobot.event.EventBus;
 import me.blackwolf12333.appcki.R;
@@ -16,63 +21,92 @@ import me.blackwolf12333.appcki.events.PollVotedEvent;
 import me.blackwolf12333.appcki.events.ShowProgressEvent;
 import me.blackwolf12333.appcki.fragments.APIFragment;
 import me.blackwolf12333.appcki.generated.poll.Poll;
+import me.blackwolf12333.appcki.generated.poll.PollOption;
 
 /**
- * A fragment representing a list of Items.
- * <p/>
- * <p/>
- * interface.
+ * A simple {@link Fragment} subclass.
  */
 public class PollFragment extends APIFragment {
+    private SwipeRefreshLayout swipeContainer;
     private RecyclerView recyclerView;
+    protected PollOptionAdapter optionAdapter = new PollOptionAdapter(new ArrayList<PollOption>());
+    protected PollResultAdapter resultAdapter = new PollResultAdapter(new ArrayList<PollOption>());
+    private VolleyPoll pollAPI = new VolleyPoll();
     private TextView question;
     private Poll currentPoll;
-    VolleyPoll pollAPI = new VolleyPoll();
+    private boolean showResults = false;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
     public PollFragment() {
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         pollAPI.getActivePoll();
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_pollitem_list, container, false);
-        question = (TextView) view.findViewById(R.id.question);
-        if(view.findViewById(R.id.list_poll) instanceof RecyclerView) {
-            this.recyclerView = (RecyclerView) view.findViewById(R.id.list_poll);
-        }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_poll, container, false);
+        question = (TextView) view.findViewById(R.id.question);
+        setupSwipeContainer(view);
+        setupRecyclerView(view);
         return view;
+    }
+
+    private void setupRecyclerView(View view) {
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(optionAdapter);
+    }
+
+    private void setupSwipeContainer(View view) {
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.refreshContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refresh();
+            }
+        });
+
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        swipeContainer.setRefreshing(true);
+    }
+
+    public void updateContent(List<PollOption> content) {
+        if (showResults) {
+            resultAdapter.clear();
+            resultAdapter.addAll(content);
+        } else {
+            optionAdapter.clear();
+            optionAdapter.addAll(content);
+        }
+        swipeContainer.setRefreshing(false);
     }
 
     public void onEventMainThread(PollEvent event) {
         EventBus.getDefault().post(new ShowProgressEvent(false));
         question.setText(event.poll.getPollItem().getTitle());
         if(event.poll.getMyVote() == null) {
-            recyclerView.setAdapter(new PollOptionAdapter(event.poll.getOptions()));
+            showResults = false;
         } else {
-            recyclerView.setAdapter(new PollResultAdapter(event.poll.getOptions()));
+            showResults = true;
+            recyclerView.setAdapter(resultAdapter);
         }
         currentPoll = event.poll;
+        updateContent(currentPoll.getOptions());
     }
 
     public void onEventMainThread(PollVotedEvent event) {
-        recyclerView.setAdapter(new PollResultAdapter(currentPoll.getOptions()));
+        resultAdapter.clear();
+        resultAdapter.addAll(currentPoll.getOptions());
+        recyclerView.setAdapter(resultAdapter);
     }
 
     @Override
     public void refresh() {
-
+        pollAPI.getActivePoll();
     }
 
     @Override
@@ -86,4 +120,5 @@ public class PollFragment extends APIFragment {
         EventBus.getDefault().unregister(this);
         super.onStop();
     }
+
 }
