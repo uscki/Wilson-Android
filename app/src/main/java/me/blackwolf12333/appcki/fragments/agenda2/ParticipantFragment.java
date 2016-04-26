8265745e2b1ab8b0b8f05b65b00b62542c8a6b54
Peter Maatman <blackwolf12333@gmail.com>
2016-04-26 18:03:12 +0200

    swipe refresh voor Agenda Participants

--- app/src/main/java/me/blackwolf12333/appcki/fragments/agenda2/ParticipantFragment.java
+++ app/src/main/java/me/blackwolf12333/appcki/fragments/agenda2/ParticipantFragment.java
@@ -2,6 +2,7 @@ package me.blackwolf12333.appcki.fragments.agenda2;
 
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -14,29 +15,39 @@ import java.util.List;
 import de.greenrobot.event.EventBus;
 import me.blackwolf12333.appcki.R;
 import me.blackwolf12333.appcki.api.VolleyAgenda;
-import me.blackwolf12333.appcki.events.AgendaEvent;
+import me.blackwolf12333.appcki.events.AgendaItemEvent;
 import me.blackwolf12333.appcki.events.ShowProgressEvent;
 import me.blackwolf12333.appcki.fragments.APIFragment;
-import me.blackwolf12333.appcki.generated.agenda.AgendaItem;
+import me.blackwolf12333.appcki.generated.agenda.AgendaParticipant;
 
 /**
- * A simple {@link APIFragment} subclass.
+ * A simple {@link Fragment} subclass.
  */
-public class AgendaFragment extends APIFragment {
+public class ParticipantFragment extends APIFragment {
     private SwipeRefreshLayout swipeContainer;
+    private List<AgendaParticipant> participants;
+    private VolleyAgenda agendaAPI = new VolleyAgenda();
     private RecyclerView recyclerView;
-    protected AgendaItemAdapter adapter = new AgendaItemAdapter(new ArrayList<AgendaItem>());
-    VolleyAgenda agendaAPI = new VolleyAgenda();
+    private Integer currentId;
+    private ParticipantAdapter adapter = new ParticipantAdapter(new ArrayList<AgendaParticipant>());
 
-    public AgendaFragment() {
-        agendaAPI.getAgendaNewer();
+
+    public ParticipantFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        currentId = getArguments().getInt("id");
+        agendaAPI.getAgendaItem(currentId);
+        super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_agenda, container, false);
+        View view = inflater.inflate(R.layout.fragment_participant2, container, false);
         setupSwipeContainer(view);
         setupRecyclerView(view);
         return view;
@@ -64,20 +75,20 @@ public class AgendaFragment extends APIFragment {
         swipeContainer.setRefreshing(true);
     }
 
-    public void updateContent(List<AgendaItem> content) {
+    public void updateContent(List<AgendaParticipant> content) {
         adapter.clear();
         adapter.addAll(content);
         swipeContainer.setRefreshing(false);
     }
 
-    public void onEventMainThread(AgendaEvent event) {
+    public void onEventMainThread(AgendaItemEvent event) {
         EventBus.getDefault().post(new ShowProgressEvent(false));
-        this.updateContent(event.agenda.getContent());
+        this.updateContent(event.agendaItem.getParticipants());
     }
 
     @Override
     public void refresh() {
-        agendaAPI.getAgendaNewer();
+        agendaAPI.getAgendaItem(currentId);
     }
 
     @Override
