ff012a24d22dfd789463e95591470e55dee1e203
Peter Maatman <blackwolf12333@gmail.com>
2016-06-28 15:35:26 +0200

    whitespace fixings

--- app/src/main/java/me/blackwolf12333/appcki/fragments/agenda/AgendaDeelnemersAdapter.java
+++ app/src/main/java/me/blackwolf12333/appcki/fragments/agenda/AgendaDeelnemersAdapter.java
@@ -2,6 +2,7 @@ package me.blackwolf12333.appcki.fragments.agenda;
 
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
