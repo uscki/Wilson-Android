13558ef218e57adddc995f293f483757d91d724c
Ty Mees <t.mees@randev.nl>
2017-01-05 21:11:06 +0100

    Added error handling for the api call error handling

--- app/src/main/java/nl/uscki/appcki/android/api/Callback.java
+++ app/src/main/java/nl/uscki/appcki/android/api/Callback.java
@@ -5,6 +5,7 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.SocketTimeoutException;
+import java.util.Date;
 
 import de.greenrobot.event.EventBus;
 import nl.uscki.appcki.android.error.ConnectionError;
@@ -42,9 +43,26 @@ public abstract class Callback<T> implements retrofit2.Callback<T> {
         try {
             Gson gson = new Gson();
             ServerError error = gson.fromJson(response.raw().body().string(), ServerError.class);
+           // ServerError error1 = new ServerError();
+            //error1.setError("I don't know man");
+            //error1.setException("This shit is weird");
+            //error1.setMessage("Tried rebooting?");
+            //error1.setPath("/api/unsolvableURL");
+            //error1.setStatus(500);
+            //error1.setTimestamp(new Date().getTime());
             EventBus.getDefault().post(new ServerErrorEvent(error));
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (IllegalStateException e)
+        {
+            ServerError error = new ServerError();
+            error.setError("Internal Server Error");
+            error.setException("java.lang.IllegalStateException");
+            error.setMessage("Error in parsing error message from server. So meta.");
+            error.setPath("");
+            error.setStatus(500);
+            error.setTimestamp(new Date().getTime());
+            EventBus.getDefault().post(new ServerErrorEvent(error));
         }
     }
 
