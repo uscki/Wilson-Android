9541af98c6fc1ed614b8b0198c53953963a533d7
Peter Maatman <blackwolf12333@gmail.com>
2017-03-27 20:00:56 +0200

    Clean up state loading in MainActivity

--- app/src/main/java/nl/uscki/appcki/android/activities/MainActivity.java
+++ app/src/main/java/nl/uscki/appcki/android/activities/MainActivity.java
@@ -64,15 +64,6 @@ public class MainActivity extends BasicActivity
 
     LoginFragment loginFragment = new LoginFragment();
 
-    // Hold a reference to the current animator,
-    // so that it can be canceled mid-way.
-    private Animator mCurrentAnimator;
-
-    // The system "short" animation time duration, in milliseconds. This
-    // duration is ideal for subtle animations or animations that occur
-    // very frequently.
-    private int mShortAnimationDuration;
-
     public enum Screen {
         LOGIN,
         NEWS,
@@ -93,6 +84,7 @@ public class MainActivity extends BasicActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         FirebaseCrash.log("Creating MainActivity");
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -108,15 +100,11 @@ public class MainActivity extends BasicActivity
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        if(savedInstanceState != null) {
-            FirebaseCrash.log("savedInstanceState != null");
-            int ord = savedInstanceState.getInt("screen");
-            Screen screen = Screen.values()[ord];
-            currentScreen = screen;
-            loadState(screen);
+        if (!UserHelper.getInstance().isLoggedIn()) {
+            initLoggedOutUI();
         } else {
-            FirebaseCrash.log("savedInstanceState == null");
-            loadState(Screen.NEWS); // load News if there is no known last screen
+            initLoggedInUI();
+            openTab(HomeFragment.NEWS);
         }
 
         if(getIntent().getAction().equals(Intent.ACTION_VIEW)) {
@@ -310,45 +298,6 @@ public class MainActivity extends BasicActivity
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
     }
 
-    private void loadState(@NonNull Screen screen) {
-        if (!UserHelper.getInstance().isLoggedIn()) {
-            FirebaseCrash.log("loadState: not logged in");
-            initLoggedOutUI();
-            return;
-        }
-        initLoggedInUI();
-
-        Log.d("MainActivity", "load: " + screen.name());
-        FirebaseCrash.log("loadState: opening screen " + screen.name());
-        openLastScreen(screen);
-    }
-
-    private void openLastScreen(Screen screen) {
-        switch (screen) {
-            case LOGIN:
-                initLoggedOutUI();
-                break;
-            case NEWS:
-                openTab(HomeFragment.NEWS);
-                break;
-            case AGENDA:
-                openTab(HomeFragment.AGENDA);
-                break;
-            case POLL_OVERVIEW:
-                openFragment(new PollOverviewFragment(), null);
-                break;
-            case ROEPHOEK:
-                openTab(HomeFragment.ROEPHOEK);
-                break;
-            case MEETING_OVERVIEW:
-                openFragment(new MeetingOverviewFragment(), null);
-                break;
-            default: // UNHANDLED SCREENS eg AGENDA_DETAIL
-                openTab(HomeFragment.NEWS);
-                break;
-        }
-    }
-
     private void buildRoephoekAddDialog() {
         DialogFragment newFragment = new RoephoekDialogFragment();
         newFragment.show(getSupportFragmentManager(), "roephoek_add");
@@ -425,142 +374,4 @@ public class MainActivity extends BasicActivity
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
-
-    /*private void zoomImageFromThumb(final Rect startBounds, Integer id) {
-        // If there's an animation in progress, cancel it
-        // immediately and proceed with this one.
-        if (mCurrentAnimator != null) {
-            mCurrentAnimator.cancel();
-        }
-
-        // Load the high-resolution "zoomed-in" image.
-        final NetworkImageView expandedImageView = (NetworkImageView) findViewById(R.id.image_zoomed);
-        expandedImageView.setDefaultImageResId(R.drawable.account);
-        expandedImageView.setImageMediaId(id, MediaAPI.MediaSize.NORMAL);
-
-        // Calculate the starting and ending bounds for the zoomed-in image.
-        // This step involves lots of math. Yay, math.
-        //final Rect startBounds = new Rect();
-        final Rect finalBounds = new Rect();
-        final Point globalOffset = new Point();
-
-        // The start bounds are the global visible rectangle of the thumbnail,
-        // and the final bounds are the global visible rectangle of the container
-        // view. Also set the container view's offset as the origin for the
-        // bounds, since that's the origin for the positioning animation
-        // properties (X, Y).
-        //thumbView.getGlobalVisibleRect(startBounds);
-        findViewById(R.id.fragment_container)
-                .getGlobalVisibleRect(finalBounds, globalOffset);
-        startBounds.offset(-globalOffset.x, -globalOffset.y);
-        finalBounds.offset(-globalOffset.x, -globalOffset.y);
-
-        // Adjust the start bounds to be the same aspect ratio as the final
-        // bounds using the "center crop" technique. This prevents undesirable
-        // stretching during the animation. Also calculate the start scaling
-        // factor (the end scaling factor is always 1.0).
-        float startScale;
-        if ((float) finalBounds.width() / finalBounds.height()
-                > (float) startBounds.width() / startBounds.height()) {
-            // Extend start bounds horizontally
-            startScale = (float) startBounds.height() / finalBounds.height();
-            float startWidth = startScale * finalBounds.width();
-            float deltaWidth = (startWidth - startBounds.width()) / 2;
-            startBounds.left -= deltaWidth;
-            startBounds.right += deltaWidth;
-        } else {
-            // Extend start bounds vertically
-            startScale = (float) startBounds.width() / finalBounds.width();
-            float startHeight = startScale * finalBounds.height();
-            float deltaHeight = (startHeight - startBounds.height()) / 2;
-            startBounds.top -= deltaHeight;
-            startBounds.bottom += deltaHeight;
-        }
-
-        // Hide the thumbnail and show the zoomed-in view. When the animation
-        // begins, it will position the zoomed-in view in the place of the
-        // thumbnail.
-        //thumbView.setAlpha(0f);
-        expandedImageView.setVisibility(View.VISIBLE);
-
-        // Set the pivot point for SCALE_X and SCALE_Y transformations
-        // to the top-left corner of the zoomed-in view (the default
-        // is the center of the view).
-        expandedImageView.setPivotX(0f);
-        expandedImageView.setPivotY(0f);
-
-        // Construct and run the parallel animation of the four translation and
-        // scale properties (X, Y, SCALE_X, and SCALE_Y).
-        AnimatorSet set = new AnimatorSet();
-        set
-                .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
-                        startBounds.left, finalBounds.left))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
-                        startBounds.top, finalBounds.top))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
-                        startScale, 1f)).with(ObjectAnimator.ofFloat(expandedImageView,
-                View.SCALE_Y, startScale, 1f));
-        set.setDuration(mShortAnimationDuration);
-        set.setInterpolator(new DecelerateInterpolator());
-        set.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                mCurrentAnimator = null;
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-                mCurrentAnimator = null;
-            }
-        });
-        set.start();
-        mCurrentAnimator = set;
-
-        // Upon clicking the zoomed-in image, it should zoom back down
-        // to the original bounds and show the thumbnail instead of
-        // the expanded image.
-        final float startScaleFinal = startScale;
-        expandedImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mCurrentAnimator != null) {
-                    mCurrentAnimator.cancel();
-                }
-
-                // Animate the four positioning/sizing properties in parallel,
-                // back to their original values.
-                AnimatorSet set = new AnimatorSet();
-                set.play(ObjectAnimator
-                        .ofFloat(expandedImageView, View.X, startBounds.left))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
-                                        View.Y, startBounds.top))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
-                                        View.SCALE_X, startScaleFinal))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
-                                        View.SCALE_Y, startScaleFinal));
-                set.setDuration(mShortAnimationDuration);
-                set.setInterpolator(new DecelerateInterpolator());
-                set.addListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        //thumbView.setAlpha(1f);
-                        expandedImageView.setVisibility(View.GONE);
-                        mCurrentAnimator = null;
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animation) {
-                        //thumbView.setAlpha(1f);
-                        expandedImageView.setVisibility(View.GONE);
-                        mCurrentAnimator = null;
-                    }
-                });
-                set.start();
-                mCurrentAnimator = set;
-            }
-        });
-    }*/
 }
