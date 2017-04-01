e1f82eaa8bb570e95b6b809e1620f3ee51a50a86
Peter Maatman <blackwolf12333@gmail.com>
2017-04-01 19:06:22 +0200

    Setup basic meeting planner notifications

--- app/src/main/java/nl/uscki/appcki/android/Utils.java
+++ app/src/main/java/nl/uscki/appcki/android/Utils.java
@@ -1,5 +1,13 @@
 package nl.uscki.appcki.android;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.widget.AppCompatDrawableManager;
+
 import org.joda.time.DateTime;
 import org.joda.time.Days;
 import org.joda.time.Hours;
@@ -43,6 +51,21 @@ public class Utils {
         } else {
             return "(± " + minutes + " minuten geleden)";
         }
+    }
+
+    public static Bitmap getBitmapFromVectorDrawable(Context context, int drawableId) {
+        Drawable drawable = AppCompatDrawableManager.get().getDrawable(context, drawableId);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            drawable = (DrawableCompat.wrap(drawable)).mutate();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
     }
 
     public static String timestampConversion(String timestamp) {
--- app/src/main/java/nl/uscki/appcki/android/services/NotificationReceiver.java
+++ app/src/main/java/nl/uscki/appcki/android/services/NotificationReceiver.java
@@ -1,8 +1,11 @@
 package nl.uscki.appcki.android.services;
 
-import android.app.Notification;
-import android.support.v4.app.NotificationCompat;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -10,6 +13,8 @@ import com.google.firebase.messaging.RemoteMessage;
 
 import nl.uscki.appcki.android.App;
 import nl.uscki.appcki.android.R;
+import nl.uscki.appcki.android.Utils;
+import nl.uscki.appcki.android.activities.MeetingActivity;
 
 /**
  * Created by peter on 3/21/17.
@@ -36,18 +41,44 @@ public class NotificationReceiver extends FirebaseMessagingService {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
 
+        String title = remoteMessage.getData().get("title");
+        String content = remoteMessage.getData().get("content");
+        String type = remoteMessage.getData().get("type");
+
+        // Because androids default BitmapFactory doesn't work with vector drawables
+        Bitmap bm = Utils.getBitmapFromVectorDrawable(getApplicationContext(), R.drawable.ic_wilson);
+
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
-        Notification n  = new NotificationCompat.Builder(App.getContext())
-                .setContentTitle(remoteMessage.getData().get("title"))
-                .setContentText(remoteMessage.getData().get("content"))
-                .setSmallIcon(R.drawable.ckilogo)
-                //.setContentIntent(pIntent)
-                .setAutoCancel(true).build();
+        NotificationCompat.Builder n  = new NotificationCompat.Builder(getApplicationContext());
+                n.setContentTitle(title)
+                .setContentText(content)
+                .setAutoCancel(true);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            n.setSmallIcon(R.drawable.ckilogo)
+                    .setLargeIcon(bm);
+        } else {
+            n.setSmallIcon(R.drawable.ckilogo);
+        }
+
+        Intent intent = null;
+
+        switch (type) {
+            case "meeting":
+                intent = new Intent(App.getContext(), MeetingActivity.class);
+                break;
+        }
+
+        if (intent != null) {
+            intent.putExtra("id", Integer.parseInt(remoteMessage.getData().get("id")));
+            intent.setAction(Intent.ACTION_VIEW);
+            PendingIntent pIntent = PendingIntent.getActivity(App.getContext(), 0, intent, 0);
+            n.setContentIntent(pIntent);
+        }
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(App.getContext());
-        notificationManager.notify(0, n);
+        notificationManager.notify(0, n.build());
     }
 
     @Override
