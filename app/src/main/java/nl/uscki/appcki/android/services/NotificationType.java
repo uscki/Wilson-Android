fd32abac41c8755b7106f45984eb1b29ef134cc8
Peter Maatman <blackwolf12333@gmail.com>
2017-04-02 16:37:22 +0200

    Add support for forum notifications

---
+++ app/src/main/java/nl/uscki/appcki/android/services/NotificationType.java
@@ -0,0 +1,22 @@
+/*
+ * B.A.D.W.O.L.F.
+ * AppCKI der USCKI Incognito
+ *
+ * Copyright (c) 2017 USCKI Incognito. All rights reserved.
+ *
+ */
+
+package nl.uscki.api.service.notifications;
+
+public enum NotificationType {
+    achievement,
+    agenda_announcement,
+    agenda_new,
+    agenda_reply,
+    forum_new_topic,
+    forum_reply,
+    other,
+    meeting_filledin,
+    meeting_new,
+    meeting_planned
+}
