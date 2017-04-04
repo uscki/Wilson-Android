a2c5fc04383774501aca915a06ba6668e7c1d256
Peter Maatman <blackwolf12333@gmail.com>
2017-04-04 15:30:22 +0200

    files created for news activity

---
+++ app/src/main/java/nl/uscki/appcki/android/activities/NewsActivity.java
@@ -0,0 +1,24 @@
+package nl.uscki.appcki.android.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import nl.uscki.appcki.android.R;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
