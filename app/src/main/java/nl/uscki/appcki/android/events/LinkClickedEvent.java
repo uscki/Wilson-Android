4cd194d36e7daeec464e26ef0364d437d7ee3437
Peter Maatman <blackwolf12333@gmail.com>
2017-04-12 10:54:02 +0200

    Undo remove of LinkClickedEvent

---
+++ app/src/main/java/nl/uscki/appcki/android/events/LinkClickedEvent.java
@@ -0,0 +1,13 @@
+package nl.uscki.appcki.android.events;
+
+/**
+ * Created by peter on 4/6/17.
+ */
+
+public class LinkClickedEvent {
+    public String url;
+
+    public LinkClickedEvent(String url) {
+        this.url = url;
+    }
+}
