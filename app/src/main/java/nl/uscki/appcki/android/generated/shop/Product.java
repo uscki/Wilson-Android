82166893eae713eb8d0f0fc67b443e407012f008
Peter Maatman <peter.maatman@rentman.nl>
2018-06-15 19:33:19 +0200

    Add shop endpoint

---
+++ app/src/main/java/nl/uscki/appcki/android/generated/shop/Product.java
@@ -0,0 +1,23 @@
+package nl.uscki.appcki.android.generated.shop;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Product {
+    @Expose
+    @SerializedName("id")
+    Integer id;
+    @Expose
+    @SerializedName("image")
+    Integer image;
+    @Expose
+    @SerializedName("price")
+    Double price;
+    @Expose
+    @SerializedName("stock")
+    Integer stock;
+    @Expose
+    @SerializedName("title")
+    String title;
+
+}
