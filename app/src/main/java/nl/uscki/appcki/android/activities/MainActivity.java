70b51c1210602c5c9d9ed20867917e97d669f26c
A.J. de Mooij <a.j.demooij@gmail.com>
2018-07-31 15:31:38 +0200

    Removed settings from detail views

--- app/src/main/java/nl/uscki/appcki/android/activities/MainActivity.java
+++ app/src/main/java/nl/uscki/appcki/android/activities/MainActivity.java
@@ -154,6 +154,7 @@ public class MainActivity extends BasicActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        menu.clear();
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
