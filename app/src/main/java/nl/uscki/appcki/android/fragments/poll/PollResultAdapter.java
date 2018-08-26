26c29902c2b16da0d8cfbfd00ba0aeee13724659
A.J. de Mooij <a.j.demooij@gmail.com>
2018-08-26 19:31:20 +0200

    Vote on your favorite poll by swiping right!

--- app/src/main/java/nl/uscki/appcki/android/fragments/poll/PollResultAdapter.java
+++ app/src/main/java/nl/uscki/appcki/android/fragments/poll/PollResultAdapter.java
@@ -1,10 +1,13 @@
 package nl.uscki.appcki.android.fragments.poll;
 
 import android.graphics.Color;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -22,9 +25,11 @@ import nl.uscki.appcki.android.views.VotesGraphView;
 
 public class PollResultAdapter extends BaseItemAdapter<PollResultAdapter.ViewHolder, PollOption> {
     int totalvotes;
+    private boolean canVote;
 
-    public PollResultAdapter(List<PollOption> items) {
+    public PollResultAdapter(List<PollOption> items, boolean canVote) {
         super(items);
+        this.canVote = canVote;
 
         for (PollOption item : items) {
             totalvotes += item.getVoteCount();
@@ -43,6 +48,7 @@ public class PollResultAdapter extends BaseItemAdapter<PollResultAdapter.ViewHol
         holder.name.setText(items.get(position).getName());
 
         try {
+            holder.setCanVote(canVote);
             holder.bar.setBarColor(Color.parseColor(items.get(position).getColor().toLowerCase()));
         } catch (Exception e) {
             e.printStackTrace();
@@ -60,6 +66,12 @@ public class PollResultAdapter extends BaseItemAdapter<PollResultAdapter.ViewHol
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
 
+        @BindView(R.id.pollOptionBackground)
+        RelativeLayout background;
+
+        @BindView(R.id.pollOptionForeground)
+        LinearLayout foreground;
+
         @BindView(R.id.poll_result_option_name)
         TextView name;
         @BindView(R.id.poll_result_option_bar)
@@ -69,6 +81,14 @@ public class PollResultAdapter extends BaseItemAdapter<PollResultAdapter.ViewHol
             super(view);
             mView = view;
             ButterKnife.bind(this, view);
+        }
+
+        public void setCanVote(boolean canVote) {
+            if(canVote) {
+                bar.setVisibility(View.INVISIBLE);
+            } else {
+                bar.setVisibility(View.VISIBLE);
+            }
         }
     }
 
