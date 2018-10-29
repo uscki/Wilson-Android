0c02f92a6b8b18039190d20b8bd5623fb4d01ade
Peter Maatman <peter.maatman@rentman.nl>
2018-10-29 15:23:20 +0100

    basic store implementation

--- app/src/main/java/nl/uscki/appcki/android/fragments/shop/StoreFragment.java
+++ app/src/main/java/nl/uscki/appcki/android/fragments/shop/StoreFragment.java
@@ -1,33 +1,59 @@
 package nl.uscki.appcki.android.fragments.shop;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
+import nl.uscki.appcki.android.activities.MainActivity;
 import nl.uscki.appcki.android.api.Callback;
 import nl.uscki.appcki.android.api.Services;
 import nl.uscki.appcki.android.fragments.RefreshableFragment;
 import nl.uscki.appcki.android.generated.common.Pageable;
 import nl.uscki.appcki.android.generated.shop.Product;
 import nl.uscki.appcki.android.generated.shop.Store;
+import nl.uscki.appcki.android.helpers.UserHelper;
 import retrofit2.Response;
 
 public class StoreFragment extends RefreshableFragment {
+    Callback<Pageable<Product>> callback = new Callback<Pageable<Product>>() {
+        @Override
+        public void onSucces(Response<Pageable<Product>> response) {
+            Log.e("storefragment", "test");
+            response.body().getContent().sort(new Comparator<Product>() {
+                @Override
+                public int compare(Product o1, Product o2) {
+                    return UserHelper.getInstance().isPreferedProduct(o1, o2);
+                }
+            });
+            for (Product product : response.body().getContent()) {
+                Log.e("storeFragment", product.title);
+            }
+            getAdapter().update(response.body().getContent());
+            StoreFragment.this.swipeContainer.setRefreshing(false);
+        }
+    };
+
+    private Integer storeId;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setAdapter(new ProductAdapter(new ArrayList<Product>()));
 
-        Services.getInstance().shopService.getProductsForStore(1).enqueue(new Callback<Pageable<Product>>() {
-            @Override
-            public void onSucces(Response<Pageable<Product>> response) {
-                getAdapter().update(response.body().getContent());
-            }
-        });
+        MainActivity.currentScreen = MainActivity.Screen.STORE_BUY;
+
+        this.storeId = getArguments().getInt("id", -1);
+
+        if (this.storeId != -1) {
+            Services.getInstance().shopService.getProductsForStore(storeId).enqueue(callback);
+        }
+
+        setAdapter(new ProductAdapter(new ArrayList<Product>()));
     }
 
     @Override
@@ -38,11 +64,6 @@ public class StoreFragment extends RefreshableFragment {
 
     @Override
     public void onSwipeRefresh() {
-        Services.getInstance().shopService.getProductsForStore(1).enqueue(new Callback<Pageable<Product>>() {
-            @Override
-            public void onSucces(Response<Pageable<Product>> response) {
-                getAdapter().update(response.body().getContent());
-            }
-        });
+        Services.getInstance().shopService.getProductsForStore(this.storeId).enqueue(callback);
     }
 }
