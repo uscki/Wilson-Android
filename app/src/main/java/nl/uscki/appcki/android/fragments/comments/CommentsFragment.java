7e4fbd7353f4bb5e87a032204bf42670f6f80e4c
A.J. de Mooij <a.j.demooij@gmail.com>
2019-01-06 09:20:27 +0100

    Comments are loaded again (was bug)

--- app/src/main/java/nl/uscki/appcki/android/fragments/comments/CommentsFragment.java
+++ app/src/main/java/nl/uscki/appcki/android/fragments/comments/CommentsFragment.java
@@ -27,6 +27,7 @@ import retrofit2.Response;
 public abstract class CommentsFragment extends PageableFragment<CommentPage> {
 
     public static final String ACTION_VIEW_COMMENTS = "nl.uscki.appcki.android.actions.MainActivity.ACTION_VIEW_COMMENTS";
+    public static final String ARGUMENT_COMMENTS_TOPIC_ID = "nl.uscki.appcki.android.comments.TOPIC_ID";
 
     private static final int COMMENTS_PAGE_SIZE = 10;
 
@@ -47,7 +48,7 @@ public abstract class CommentsFragment extends PageableFragment<CommentPage> {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if(getArguments() != null) {
-            commentOnTopicId = getArguments().getInt("id");
+            commentOnTopicId = getArguments().getInt(ARGUMENT_COMMENTS_TOPIC_ID);
         }
     }
 
