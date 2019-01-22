775c82d658f576ef970abe410cb5c5aab98684b3
A.J. de Mooij <a.j.demooij@gmail.com>
2019-01-22 18:48:52 +0100

    Agenda-related notifications take you straight to correct tab

--- app/src/main/java/nl/uscki/appcki/android/helpers/notification/agenda/AgendaFromBackupNotification.java
+++ app/src/main/java/nl/uscki/appcki/android/helpers/notification/agenda/AgendaFromBackupNotification.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 
 import com.google.firebase.messaging.RemoteMessage;
 
+import nl.uscki.appcki.android.activities.AgendaActivity;
+
 public class AgendaFromBackupNotification extends ReproducibleAgendaActionNotification {
 
     public AgendaFromBackupNotification(Context c, RemoteMessage message) {
@@ -17,5 +19,13 @@ public class AgendaFromBackupNotification extends ReproducibleAgendaActionNotifi
 
     public AgendaFromBackupNotification(Context c, Intent intent, boolean allowSubscribe, boolean allowExport) {
         super(c, intent, allowSubscribe, allowExport);
+    }
+
+    @Override
+    protected Intent getNotificationIntent() {
+        Intent intent = new Intent(this.context, AgendaActivity.class);
+        intent.putExtra(AgendaActivity.PARAM_AGENDA_ID, this.id);
+        intent.setAction(AgendaActivity.ACTION_AGENDA_PARTICIPANTS);
+        return intent;
     }
 }
