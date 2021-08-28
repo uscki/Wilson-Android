2f639547922ef454ee0e39a60017c3ec2c524031
A.J. de Mooij <a.j.demooij@gmail.com>
2021-08-28 19:57:08 +0200

    Finished mentor tree.
    Previously, we did not, because Peter Maatman proved difficult.
    He has 6 parents, two of which are Tjeu, two of the others of which
    are Kambiz. He is a child of those two (four?) multiple times himself.
    I don't care.
    
    On the USCKI website, more information about mentor groups is available,
    such as an introductory text, and the starting year. This is not in the
    API, so left for future work.

--- app/src/main/java/nl/uscki/appcki/android/fragments/smobo/SmoboMentorTreeFragment.java
+++ app/src/main/java/nl/uscki/appcki/android/fragments/smobo/SmoboMentorTreeFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import org.joda.time.DateTime;
 
@@ -29,13 +30,16 @@ import nl.uscki.appcki.android.generated.smobo.SmoboMentorNode;
 public class SmoboMentorTreeFragment extends Fragment {
 
     private SmoboActivity activity;
+    private SwipeRefreshLayout swipeContainer;
     private TextView startYear;
+    private TextView startedYearsAgo;
     private RecyclerView parents;
     private RecyclerView siblings;
     private RecyclerView childrenContainer;
 
     private SmoboSearchResultAdapter parentAdapter;
     private SmoboSearchResultAdapter siblingAdapter;
+    private SmoboChildNodeAdapter childNodeAdapter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -51,7 +55,9 @@ public class SmoboMentorTreeFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_smobo_mentor_tree, container, false);
+        this.swipeContainer = view.findViewById(R.id.smobo_mentor_tree_swipe_container);
         this.startYear = view.findViewById(R.id.smobo_mentor_tree_startyear);
+        this.startedYearsAgo = view.findViewById(R.id.smobo_mentor_tree_started_years_ago);
         this.parents = view.findViewById(R.id.smobo_mentor_tree_parents_recycler_view);
         this.siblings = view.findViewById(R.id.smobo_mentor_tree_siblings_recycler_view);
         this.childrenContainer = view.findViewById(R.id.smobo_mentor_tree_children_recycler_view);
@@ -63,6 +69,8 @@ public class SmoboMentorTreeFragment extends Fragment {
         this.parents.setAdapter(this.parentAdapter);
         this.siblings.setAdapter(this.siblingAdapter);
 
+        setupSwipeContainer();
+
         if(this.activity != null && this.activity.getP() != null) {
             setupItem(this.activity.getP());
         }
@@ -71,16 +79,35 @@ public class SmoboMentorTreeFragment extends Fragment {
     }
 
     private void setupItem(SmoboItem person) {
+        this.swipeContainer.setRefreshing(false);
         setupStartYear(person.getGroups());
-        setupMentorTree(person.getMentorNode());
+        setupMentorTree(person.getMentorNode().getSubnodes());
         this.parentAdapter.clear();
         this.parentAdapter.addItems(person.getMentorNode().getParents());
         this.siblingAdapter.clear();
         this.siblingAdapter.addItems(person.getMentorNode().getChildren());
     }
 
-    private void setupMentorTree(SmoboMentorNode person) {
+    private void setupMentorTree(List<SmoboMentorNode> subNodes) {
+        if(subNodes != null && subNodes.size() > 0) {
+            this.childNodeAdapter = new SmoboChildNodeAdapter(this.activity.getP().getPerson(), subNodes);
+            this.childrenContainer.setAdapter(this.childNodeAdapter);
+            this.childrenContainer.setLayoutManager(new LinearLayoutManager(getContext()));
+            this.childrenContainer.setVisibility(View.VISIBLE);
+        } else {
+            this.childrenContainer.setVisibility(View.GONE);
+        }
+    }
 
+    protected void setupSwipeContainer() {
+        swipeContainer.setOnRefreshListener(() -> this.activity.refreshSmoboItem());
+
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        swipeContainer.setRefreshing(false);
     }
 
     private void setupStartYear(List<Committee> groups) {
@@ -95,6 +122,12 @@ public class SmoboMentorTreeFragment extends Fragment {
         }
         if(firstYear != null) {
             this.startYear.setText(firstYear.toString("Y"));
+            this.startedYearsAgo.setText(
+                    getString(
+                            R.string.smobo_mentortree_formatted_years_ago,
+                            new DateTime().getYear() - firstYear.getYear()
+                    )
+            );
         }
     }
 
